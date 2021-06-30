575f4dcddebe2e4bb38b0a0e63c9133cde06f625
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-30 15:07:42 -0400

    added slugs to all models

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
     belongs_to :artist
     has_many :songGenres
     has_many :genres, through: :songGenres
+
+    def slug 
+        lowercase = self.name.downcase
+        lowercase.split(" ").join('-')
+    end
+
+    def self.find_by_slug(slug)
+        Song.all.find {|song| song.slug == slug}
+    end
 end 
