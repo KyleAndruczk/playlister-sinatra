28d07f6cf648288ad979071f005afdd2589f439b
Bill <billfeng2002@gmail.com>
2021-06-30 12:48:15 -0700

    fixed new

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,17 +9,25 @@ class SongsController < ApplicationController
         erb :"songs/new"
     end
 
+    patch "/songs/:slug/edit" do
+
+    end
+
     get "/songs/:slug" do
         @song = Song.find_by_slug(params[:slug])
         erb :"songs/show"
     end
 
     post "/songs" do
+        artist=Artist.find_by(name: params[:artist][:name])
+        params[:artist_id]=artist.id
         song=Song.create(params[:song])
-        params[:genres].each{
-            |genre|
-            SongGenre.create(song_id: song.id, genre_id: genre[:id])
-        }
+        if(params[:genres]!=nil)
+            params[:genres].each{
+                |genre|
+                SongGenre.create(song_id: song.id, genre_id: genre[:id])
+            }
+        end
 
         redirect "/songs"
     end
