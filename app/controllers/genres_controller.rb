c50a1f459154905e3713c56e5bd44b0dfd511b43
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-30 15:39:22 -0400

    fixed show.erb

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,7 +5,7 @@ class GenresController < ApplicationController
     end 
 
     get "/genres/:slug" do
-        @song = Genre.find_by_slug
+        @song = Genre.find_by_slug(params[:slug])
         erb :"genres/show"
     end
 end
