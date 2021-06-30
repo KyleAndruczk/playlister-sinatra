5697d3269eb9160ea99bf1ef23115c0000fc618b
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-30 15:23:46 -0400

    fixed depreciated spelling of song_genres

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveRecord::Base
-    has_many :songGenres
-    has_many :songs, through: :songGenres
+    has_many :song_genres
+    has_many :songs, through: :song_genres
     has_many :artists, through: :songs
 
     def slug 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
     belongs_to :artist
-    has_many :songGenres
-    has_many :genres, through: :songGenres
+    has_many :song_genres
+    has_many :genres, through: :song_genres
 
     def slug 
         lowercase = self.name.downcase
